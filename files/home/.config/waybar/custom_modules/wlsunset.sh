8888501b80f23756e8cb697f7f13a8b8fba1e4a6
Dustin Bragg <dustinlbragg@gmail.com>
2024-03-27 21:06:49 -0500

    Updated night light icons

--- files/home/.config/waybar/custom_modules/wlsunset.sh
+++ files/home/.config/waybar/custom_modules/wlsunset.sh
@@ -3,9 +3,9 @@
 sleep 0.25s
 cur_wlsunset=$(pidof wlsunset)
 if [ $? == 0 ]; then
-	echo Light 
+	echo Light 
 	echo "Night Light ON"
 else
-	echo Light 
+	echo Light 
 	echo "Night Light OFF"
 fi
